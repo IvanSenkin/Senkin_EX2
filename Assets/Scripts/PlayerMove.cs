758e94f8d857105cc85a1bb7f867763f8db36293
IvanSenkin <83362469+IvanSenkin@users.noreply.github.com>
2021-07-16 15:16:38 +0300

    commit

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform cameraLook;
+    [SerializeField] private Transform headLook;
+    [SerializeField] private int _maxHP;
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float sensitivityX;
+    [SerializeField] private Animator _animator;
+    public static Action<int> changeHP;
+    private int _hp;
+    private float moeuseLookX;
+    private float xRotation;
+    private Vector3 dir;
+    
+    private void Start()
+    {
+  
+    }
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _hp = _maxHP;
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+    }
+    private void Update()
+    {
+        PlayerLook();
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {    
+            _animator.SetTrigger("Fire");
+            TakeDamage(10);   // атачим пока сами себя 
+           // Fire();
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Time.timeScale = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = true;
+           // Time.timeScale = 0;
+            cameraLook.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+    }
+    private void PlayerLook()
+    {             
+            moeuseLookX = Input.GetAxis("Mouse X");
+        var moeuseLookY = Input.GetAxis("Mouse Y") * sensitivityX * Time.deltaTime;
+        transform.Rotate(0f, moeuseLookX * sensitivityX * Time.deltaTime, 0f);
+
+        xRotation -= moeuseLookY;
+        xRotation = Mathf.Clamp(xRotation, -30f, 10f);
+        headLook.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        cameraLook.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+    private void FixedUpdate()
+    {
+        if (dir != Vector3.zero)
+        {            
+            var speed = dir * _speed * Time.fixedDeltaTime;
+            transform.Translate(speed);
+            _animator.SetBool("Run", true);
+            dir = Vector3.zero;  
+        }
+       
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            var speed2 = dir * _speed * 2 * Time.fixedDeltaTime;
+            transform.Translate(speed2);
+            dir = Vector3.zero;
+        }
+          else 
+          {
+            _animator.SetBool("Run", false);
+            _animator.SetBool("FastRun", false);
+          }
+    }
+    public void TakeDamage(int damage)
+    {
+        changeHP?.Invoke(_hp);
+        Debug.Log("Aaa");
+        _hp -= damage;
+        if (_hp <= 0)
+        {
+            _animator.SetTrigger("Death");
+        }
+    }
+
+}
+
+
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] public Image _hpBar;
+    public float _fill;
+    public Color myColor;
+   // [SerializeField] public Texture2D icon;
+   
+    private void Start()
+    {
+        _fill = 1f;
+    }
+    private void Awake()
+    {
+        PlayerMove.changeHP += UpdateHP;
+    }
+    private void UpdateHP(int hp)
+    {
+        _hpBar.fillAmount = hp * 0.01f; ;
+    }
+    void OnGUI()
+    {
+        myColor = RGBSlider(new Rect(10, 10, 100, 20), myColor); 
+       //GUI.Box(new Rect(Screen.width -120 , 10, 90, 20), icon);
+        
+    }
+    Color RGBSlider(Rect screenRect, Color rgb)
+    {
+        rgb.r = LabelSlider(screenRect, rgb.r, 1.0f, "Red");
+        screenRect.y += 20;
+        rgb.g = LabelSlider(screenRect, rgb.g, 1.0f, "Green");
+        screenRect.y += 20;
+        rgb.b = LabelSlider(screenRect, rgb.b, 1.0f, "Blue");
+        screenRect.y += 20;
+        rgb.a = LabelSlider(screenRect, rgb.a, 1.0f, "rgb.a");
+
+        return rgb;
+    }
+    float LabelSlider(Rect screenRect, float sliderValue, float sliderMaxValue, string labelText)
+    {
+        GUI.Label(screenRect, labelText);
+        screenRect.x += screenRect.width;
+        sliderValue = GUI.HorizontalSlider(screenRect, sliderValue, 0.0f, sliderMaxValue);
+        return sliderValue;
+    }
+}
